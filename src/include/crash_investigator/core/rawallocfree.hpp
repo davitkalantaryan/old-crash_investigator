308e53662a622358f4e9a2a75aad309def638542
Davit Kalantaryan <davit.kalantaryan@desy.de>
2021-12-05 12:02:20 +0100

    fixed cases with investigation without C

--- src/include/crash_investigator/core/rawallocfree.hpp
+++ src/include/crash_investigator/core/rawallocfree.hpp
@@ -22,9 +22,11 @@ namespace crash_investigator {
 static inline void* mallocn  ( size_t a_count ) CRASH_INVEST_NOEXCEPT  {return :: malloc(a_count);}
 static inline void* reallocn ( void* a_ptr, size_t a_count ) CRASH_INVEST_NOEXCEPT { return :: realloc(a_ptr,a_count); }
 static inline void* callocn  ( size_t a_nmemb, size_t a_size ) CRASH_INVEST_NOEXCEPT { return ::calloc(a_nmemb, a_size); }
-static inline void  freen    ( void* a_ptr ) CRASH_INVEST_NOEXCEPT { return :: free(a_ptr); }
-static inline bool  SystemSpecificEarlyRealloc  ( void*, size_t, void** a_ppReturn ) CRASH_INVEST_NODISCARD {*a_ppReturn=CRASH_INVEST_NULL;return false;}
-static inline bool  SystemSpecificEarlyDealloc( void* ) CRASH_INVEST_NOEXCEPT {return false;}
+static inline void  freen    ( void* a_ptr ) CRASH_INVEST_NOEXCEPT { :: free(a_ptr); }
+static inline bool  SystemSpecificLibInitialRealloc  ( void*, size_t, void** a_ppReturn ) CRASH_INVEST_NODISCARD {*a_ppReturn=CRASH_INVEST_NULL;return false;}
+static inline bool  SystemSpecificLibInitialDealloc( void* ) CRASH_INVEST_NOEXCEPT {return false;}
+static inline void* SystemSpecificGlibcRealloc( void* a_ptr, size_t a_count ) CRASH_INVEST_NODISCARD { return :: realloc(a_ptr,a_count); }
+static inline void  SystemSpecificGlibcDealloc( void* a_ptr ) CRASH_INVEST_NOEXCEPT { :: free(a_ptr); }
 #else
 CRASH_INVEST_DLL_PRIVATE void* mallocn  ( size_t a_count ) CRASH_INVEST_NODISCARD;
 CRASH_INVEST_DLL_PRIVATE void* reallocn ( void* a_ptr, size_t a_count ) CRASH_INVEST_NODISCARD;
